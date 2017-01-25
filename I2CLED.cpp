f44024375fcc734bc57992993c181fc2dac887b8
joshLariviere <jllhockey99@gmail.com>
2017-01-25 08:52:01 -0500

    Update I2CLED.cpp

--- I2CLED.cpp
+++ I2CLED.cpp
@@ -3,22 +3,9 @@
 #include <Timer.h>
 #include <Talon.h>
 
-/**
- * This sample program shows how to control a motor using a joystick. In the
- * operator control part of the program, the joystick is read and the value is
- * written to the motor.
- *
- * Joystick analog values range from -1 to 1 and speed controller inputs as
- * range from -1 to 1 making it easy to work together. The program also delays a
- * short time in the loop to allow other threads to run. This is generally a
- * good idea, especially since the joystick values are only transmitted from the
- * Driver Station once every 20ms.
- */
 class Robot: public frc::SampleRobot {
 public:
-	/**
-	 * Runs the motor from the output of a Joystick.
-	 */
+
 	private:
 	// Initialize Joystick on port 0
 	//frc::Joystick m_stick { 0 };
@@ -32,7 +19,6 @@ public:
 			/* Set the motor controller's output. This takes a number from -1
 			 * (100% speed in reverse) to +1 (100% speed forwards).
 			 */
-			//int whatever = NULL;
 			//uint8_t value;
 
 			ChangeColor(1);
@@ -65,14 +51,9 @@ public:
 	}
 
 
-I2C Wire { I2C::Port::kOnboard,1 };
+	I2C Wire { I2C::Port::kOnboard,1 }; //Initialize the I2C wire communication on the roborio built in I2C port and gives it an address 
 
-	/* The motor to control with the Joystick. This uses a Talon speed
-	 * controller; use the Victor or Jaguar classes for other speed controllers.
-	 *
-	 * Initialize the Talon on channel 0
-	 */
-
+	
 	frc::Talon rfm { 1 };
 
 
